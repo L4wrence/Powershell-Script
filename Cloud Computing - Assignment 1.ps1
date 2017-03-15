1b2911208e571901cc21fd737deec311c1264697
L4wrence <lawrencemathurin@googlemail.com>
2017-03-15 12:19:58 +0000

    Sarted working on creating a new resource group

--- Cloud Computing - Assignment 1.ps1
+++ Cloud Computing - Assignment 1.ps1
@@ -1,2 +1,6 @@
 ﻿#Prompts the user to log into their Azure account
 Add-AzureRmAccount
+
+$ResourceGroup = Read-Host -Prompt "What would you like to name the new Resource Group"
+
+New-AzureRmResourceGroup -Name $ResourceGroup -Location "West Europe"
