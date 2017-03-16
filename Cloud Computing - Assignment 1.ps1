5a6ce865530ccee734eabfe218dc3a70af113919
L4wrence <lawrencemathurin@googlemail.com>
2017-03-16 10:20:11 +0000

    Changed variables and started if statement for creating new rg

--- Cloud Computing - Assignment 1.ps1
+++ Cloud Computing - Assignment 1.ps1
@@ -1,6 +1,8 @@
 ﻿#Prompts the user to log into their Azure account
 Add-AzureRmAccount
 
-$ResourceGroup = Read-Host -Prompt "What would you like to name the new Resource Group"
+$Rg = Read-Host -Prompt "What would you like to name the new Resource Group"
 
-New-AzureRmResourceGroup -Name $ResourceGroup -Location "West Europe"
+if () {
+New-AzureRmResourceGroup -Name $Rg -Location "West Europe"
+}
