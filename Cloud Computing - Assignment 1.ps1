457df753d91a835e5feab49040e12799302df7af
L4wrence <lawrencemathurin@googlemail.com>
2017-03-16 21:52:29 +0000

    Completed if statement for creating resource group

--- Cloud Computing - Assignment 1.ps1
+++ Cloud Computing - Assignment 1.ps1
@@ -1,8 +1,16 @@
 ﻿#Prompts the user to log into their Azure account
 Add-AzureRmAccount
 
-$Rg = Read-Host -Prompt "What would you like to name the new Resource Group"
+$rg = Read-Host -Prompt "What would you like to name the new Resource Group"
 
-if () {
-New-AzureRmResourceGroup -Name $Rg -Location "West Europe"
+#Check to see if Resource Group is avaliable or not, if it is then create a new resource group with the given name,
+#if not then prompt the user to give a new name and create the resource group with that name 
+#https://msdn.microsoft.com/en-us/library/mt603831.aspx
+if (!(Get-AzureRmResourceGroup -Name $rg)) 
+{
+    New-AzureRmResourceGroup -Name $rg -Location "West Europe"
+} 
+else {
+    $rg = Read-Host -Prompt "Resouce Group name not avaliable, please select another"
+    New-AzureRmResourceGroup -Name $rg -Location "West Europe"
 }
