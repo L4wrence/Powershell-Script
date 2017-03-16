549e2ef73d030df0acb26f9f3a8223a54c9760ff
L4wrence <lawrencemathurin@googlemail.com>
2017-03-16 22:15:01 +0000

    changed to a do while loop

--- Cloud Computing - Assignment 1.ps1
+++ Cloud Computing - Assignment 1.ps1
@@ -1,16 +1,24 @@
 ﻿#Prompts the user to log into their Azure account
 Add-AzureRmAccount
 
-$rg = Read-Host -Prompt "What would you like to name the new Resource Group"
+#$rg = Read-Host -Prompt "What would you like to name the new Resource Group"
 
 #Check to see if Resource Group is avaliable or not, if it is then create a new resource group with the given name,
 #if not then prompt the user to give a new name and create the resource group with that name 
 #https://msdn.microsoft.com/en-us/library/mt603831.aspx
-if (!(Get-AzureRmResourceGroup -ResourceGroupName $rg -ErrorAction Ignore)) 
+#https://msdn.microsoft.com/en-us/library/mt603739.aspx
+#if (!(Get-AzureRmResourceGroup -ResourceGroupName $rg -ErrorAction Ignore)) 
+#{
+#    New-AzureRmResourceGroup -ResourceGroupName $rg -Location "West Europe"
+#} 
+#else {
+#   $rg = Read-Host -Prompt "Resouce Group name not avaliable, please select another"
+#   New-AzureRmResourceGroup -ResourceGroupName $rg -Location "West Europe"
+#}
+
+do
 {
-    New-AzureRmResourceGroup -ResourceGroupName $rg -Location "West Europe"
-} 
-else {
-    $rg = Read-Host -Prompt "Resouce Group name not avaliable, please select another"
+    $rg = Read-Host -Prompt "What would you like to name the new Resource Group"
     New-AzureRmResourceGroup -ResourceGroupName $rg -Location "West Europe"
 }
+while (!(Get-AzureRmResourceGroup -ResourceGroupName $rg -ErrorAction Ignore))
