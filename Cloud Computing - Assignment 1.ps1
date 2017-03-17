1f2b84452386073a5f2bd4f2b6fe704d7bf3e598
L4wrence <lawrencemathurin@googlemail.com>
2017-03-17 15:35:17 +0000

    more changes

--- Cloud Computing - Assignment 1.ps1
+++ Cloud Computing - Assignment 1.ps1
@@ -7,18 +7,46 @@ Add-AzureRmAccount
 #if not then prompt the user to give a new name and create the resource group with that name 
 #https://msdn.microsoft.com/en-us/library/mt603831.aspx
 #https://msdn.microsoft.com/en-us/library/mt603739.aspx
-#if (!(Get-AzureRmResourceGroup -ResourceGroupName $rg -ErrorAction Ignore)) 
+<#do
+{
+    $rg = Read-Host -Prompt "What would you like to name the new Resource Group"
+    if (!(Get-AzureRmResourceGroup -ResourceGroupName $rg -ErrorAction Ignore)) 
+    {
+        New-AzureRmResourceGroup -ResourceGroupName $rg -Location "West Europe"
+    } 
+    else {
+        $rg = Read-Host -Prompt "Resouce Group name not avaliable, please select another"
+        New-AzureRmResourceGroup -ResourceGroupName $rg -Location "West Europe"
+    }
+    
+}
+while (!(Get-AzureRmResourceGroup -ResourceGroupName $rg -ErrorAction Ignore))
+#>
+#do
 #{
-#    New-AzureRmResourceGroup -ResourceGroupName $rg -Location "West Europe"
-#} 
-#else {
-#   $rg = Read-Host -Prompt "Resouce Group name not avaliable, please select another"
-#   New-AzureRmResourceGroup -ResourceGroupName $rg -Location "West Europe"
+#    $rg = Read-Host -Prompt "What would you like to name the new Resource Group"
+#     New-AzureRmResourceGroup -ResourceGroupName $rg -Location "West Europe"
 #}
+#while (!(Get-AzureRmResourceGroup -ResourceGroupName $rg -ErrorAction Ignore))
 
 do
 {
-    $rg = Read-Host -Prompt "What would you like to name the new Resource Group"
+    if (!(Get-AzureRmResourceGroup -ResourceGroupName $rg -ErrorAction Ignore))
+    {
+        Write-Host -Prompt "Resouce Group name not avaliable"
+        
+    }
+
+    $rg = Read-Host -Prompt "What would you like to name the new Resource Group?"
     New-AzureRmResourceGroup -ResourceGroupName $rg -Location "West Europe"
 }
-while (!(Get-AzureRmResourceGroup -ResourceGroupName $rg -ErrorAction Ignore))
+until (Get-AzureRmResourceGroup -ResourceGroupName $rg)
+
+do
+{
+    # coming round from a previous loop, $num exists, indicating this is a retry.
+    if ($null -ne $num) { Write-Host "Sorry, try again" }
+
+    [int]$num = Read-Host "Enter a number"
+
+} until ($num -gt 10)
