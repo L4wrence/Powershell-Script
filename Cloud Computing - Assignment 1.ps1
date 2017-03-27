eb68f2f3069ae42cf2fd8dde30318e6af9914028
L4wrence <lawrencemathurin@googlemail.com>
2017-03-27 21:51:04 +0100

    Update to comments

--- Cloud Computing - Assignment 1.ps1
+++ Cloud Computing - Assignment 1.ps1
@@ -1,9 +1,10 @@
-﻿#Prompts the user to log into their Azure account
+﻿#Blob srorage http://derekfoster.cloudapp.net/cc2017/workshop4.htm
+
+#The user first needs to login in to their Azure account before any thing can be created
 Add-AzureRmAccount
 
-#Checks if the Resource Group name is avalibale and will prmpt the user to keep trying a new name until they input one that is avaliable
+#A Resource group needs to be created. We will check that the resource group name that they want to use is avaliable or not before attempting to create it to avoid any errors
 #https://msdn.microsoft.com/en-us/library/mt603831.aspx
-#https://msdn.microsoft.com/en-us/library/mt603739.aspx
 do
 {
     if ($null -ne $rg)
@@ -17,8 +18,11 @@ do
 until (!(Get-AzureRmResourceGroup -ResourceGroupName $rg -ErrorAction Ignore))
 
 #Creates a new Resource Group using the name that was checked against the Azure account
+#https://msdn.microsoft.com/en-us/library/mt603739.aspx
 New-AzureRmResourceGroup -ResourceGroupName $rg -Location "West Europe"
 
+#Before any webapps can be deployed an App service plan is needed, We will check to see if the App Service Plan name is avalible or not before attempting to create it
+#https://docs.microsoft.com/en-us/powershell/resourcemanager/azurerm.websites/v2.1.0/get-azurermappserviceplan
 do
 {
     if ($null -ne $asp)
@@ -29,16 +33,52 @@ do
     $asp = Read-Host -Prompt "What would you like to name the new App Service Plan?"
 
 }
-until (!(Get-AzureRmAppServicePlan -Name $asp -ErrorAction Ignore))
+until (!(Get-AzureRmAppServicePlan -ResourceGroupName $rg -Name $asp -ErrorAction Ignore))
 
 #Creates a new App Service Plan
 #https://docs.microsoft.com/en-us/powershell/resourcemanager/AzureRM.Websites/v2.1.0/new-azurermappserviceplan
 New-AzureRmAppServicePlan -ResourceGroupName $rg -Name $asp -location "West Europe"
 
-#https://docs.microsoft.com/en-us/powershell/resourcemanager/AzureRM.Websites/v2.1.0/new-azurermwebapp
-#New-AzureRmWebApp -ResourceGroupName $rg
-
+#Before any webapps can be created we need to check if the name the user wants to call the web app is avaliable
 #https://docs.microsoft.com/en-us/powershell/resourcemanager/AzureRM.Websites/v2.1.0/get-azurermwebapp
-#Get-AzureRmWebApp -ResourceGroupName $rg
+do
+{
+    if ($null -ne $wa1)
+    {
+        Write-Host "Sorry Web App name taken, try again"
+    }
+
+    $wa1 = Read-Host -Prompt "What would you like to name the new Web App?"
+
+}
+until (!(Get-AzureRmWebApp -Name $wa1 -ResourceGroupName $rg -ErrorAction Ignore))
+
+#https://docs.microsoft.com/en-us/powershell/resourcemanager/AzureRM.Websites/v2.1.0/new-azurermwebapp
+New-AzureRmWebApp -ResourceGroupName $rg -AppServicePlan $asp -Name $wa2 -Location "West Europe"
+
+do
+{
+    if ($null -ne $wa2)
+    {
+        Write-Host "Sorry Web App name taken, try again"
+    }
+
+    $wa2 = Read-Host -Prompt "What would you like to name the new Web App?"
+
+}
+until (!(Get-AzureRmWebApp -Name $wa2 -ResourceGroupName $rg -ErrorAction Ignore))
+
+New-AzureRmWebApp -ResourceGroupName $rg -AppServicePlan $asp -Name $wa2 -Location "West Europe"
+
+$webapp1 = "http://$wa1.azurewebsites.net"
+$webapp2 = "http://$wa2.azurewebsites.net"
+
+#Opens the newly created webapps in internet explorer
+#https://blogs.msdn.microsoft.com/powershell/2006/09/10/controlling-internet-explorer-object-from-powershell/
+$IE = new-object -com internetexplorer.application
+$IE.navigate2($webapp1)
+$IE.visible = $true
+
+Get-AzureRmWebApp -ResourceGroupName $rg
 
 #https://github.com/Microsoft/azure-docs/blob/master/articles/app-service-web/app-service-web-app-azure-resource-manager-powershell.md
